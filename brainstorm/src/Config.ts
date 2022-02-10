46a203995c042a3e142d4efc031585a72e6d42d8
jmasengeshomsft <86074746+jmasengeshomsft@users.noreply.github.com>
2022-02-10 04:52:28 +0000

    enabled secure connection

--- brainstorm/src/Config.ts
+++ brainstorm/src/Config.ts
@@ -17,14 +17,16 @@ export const userConfig = {
     name: getRandomName(),
 };
 
-export const connectionConfig: AzureClientProps = useAzure ? { connection: {
-    tenantId: "YOUR-TENANT-ID-HERE",
-    tokenProvider: new AzureFunctionTokenProvider("AZURE-FUNCTION-URL" + "/api/GetAzureToken", { userId: "test-user", userName: "Test User" }),
-    orderer: "ENTER-ORDERER-URL-HERE",
-    storage: "ENTER-STORAGE-URL-HERE",
-}} : { connection: {
-    tenantId: "24045716-59f0-4cfb-aa78-0b05c21147b5", //LOCAL_MODE_TENANT_ID,
-    tokenProvider: new InsecureTokenProvider("861cb98ab994bf97ae85b9eed2f4872b", userConfig),
+export const connectionConfig: AzureClientProps =  { connection: {
+    tenantId: "24045716-59f0-4cfb-aa78-0b05c21147b5",
+    tokenProvider: new AzureFunctionTokenProvider("https://frs-brainstorm-tokenprovider.azurewebsites.net/api/jm-frs-tokenprovider" + "/api/GetAzureToken", { userId: "test-user", userName: "Test User" }),
     orderer: "https://alfred.westus2.fluidrelay.azure.com",
     storage: "https://historian.westus2.fluidrelay.azure.com",
-}} ;
+}} 
+
+// : { connection: {
+//     tenantId: "24045716-59f0-4cfb-aa78-0b05c21147b5", //LOCAL_MODE_TENANT_ID,
+//     tokenProvider: new InsecureTokenProvider("861cb98ab994bf97ae85b9eed2f4872b", userConfig),
+//     orderer: "https://alfred.westus2.fluidrelay.azure.com",
+//     storage: "https://historian.westus2.fluidrelay.azure.com",
+// }} ;
