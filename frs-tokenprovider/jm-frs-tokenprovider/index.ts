b4754e7379066a8b0b52730b552aa067c050eec4
jmasengeshomsft <86074746+jmasengeshomsft@users.noreply.github.com>
2022-02-10 01:57:44 +0000

    added the function to grab the key

--- frs-tokenprovider/jm-frs-tokenprovider/index.ts
+++ frs-tokenprovider/jm-frs-tokenprovider/index.ts
@@ -1,17 +1,57 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { ScopeType } from "@fluidframework/azure-client";
+import { generateToken } from "@fluidframework/azure-service-utils";
+
+// NOTE: retrieve the key from a secure location.
+const key = "myTenantKey";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    // tenantId, documentId, userId and userName are required parameters
+    const tenantId = (req.query.tenantId || (req.body && req.body.tenantId)) as string;
+    const documentId = (req.query.documentId || (req.body && req.body.documentId)) as string;
+    const userId = (req.query.userId || (req.body && req.body.userId)) as string;
+    const userName = (req.query.userName || (req.body && req.body.userName)) as string;
+    const scopes = (req.query.scopes || (req.body && req.body.scopes)) as ScopeType[];
+
+    if (!tenantId) {
+        context.res = {
+            status: 400,
+            body: "No tenantId provided in query params",
+        };
+        return;
+    }
+
+    if (!key) {
+        context.res = {
+            status: 404,
+            body: `No key found for the provided tenantId: ${tenantId}`,
+        };
+        return;
+    }
+
+    if (!documentId) {
+        context.res = {
+            status: 400,
+            body: "No documentId provided in query params"
+        };
+        return;
+    }
+
+    let user = { name: userName, id: userId };
+
+    // Will generate the token and returned by an ITokenProvider implementation to use with the AzureClient.
+    const token = generateToken(
+        tenantId,
+        key,
+        scopes ?? [ScopeType.DocRead, ScopeType.DocWrite, ScopeType.SummaryWrite],
+        documentId,
+        user
+    );
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        status: 200,
+        body: token
     };
-
 };
 
 export default httpTrigger;
